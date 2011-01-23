e9ffded1ebbaf886cbf6c5e765cd7d2f7ead7a1b
mushfiq <mushfiq.it@gmail.com>
2011-01-24 00:30:13 +0600

    configured basic seetings for the news list app

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -7,7 +7,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^website/', include('website.foo.urls')),
-
+    (r'^website/newslist/', 'website.newslist.views.home'), 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
